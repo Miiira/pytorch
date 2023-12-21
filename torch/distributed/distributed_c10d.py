4e66209b3e98ebeb423dd01b3b049209c6ac3f87
fduwjj <fduwjj@gmail.com>
2023-12-20 20:33:09 -0800

    Add hashing for before and after collective call
    ghstack-source-id: a5cd55ffdef2fef37669c50bccf942600e5fb8bb
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/113238

--- torch/distributed/distributed_c10d.py
+++ torch/distributed/distributed_c10d.py
@@ -2209,11 +2209,18 @@ def _object_to_tensor(obj, device):
     # Otherwise, it will casue 100X slowdown.
     # See: https://github.com/pytorch/pytorch/issues/65696
     byte_tensor = torch.ByteTensor(byte_storage).to(device)
+    if get_debug_level() == DebugLevel.DETAIL and is_nccl_available():
+        hash = torch._C._distributed_c10d._hash_tensors([byte_tensor])
+        logger.warning(f"_object_to_tensor size: {byte_tensor.numel()} hash value: {hash}")  # noqa: G004
     local_size = torch.LongTensor([byte_tensor.numel()]).to(device)
     return byte_tensor, local_size
 
 
 def _tensor_to_object(tensor, tensor_size):
+    if get_debug_level() == DebugLevel.DETAIL and is_nccl_available():
+        cuda_tensor = tensor.cuda()
+        hash = torch._C._distributed_c10d._hash_tensors([cuda_tensor])
+        logger.warning(f"_tensor_to_object size: {cuda_tensor.numel()} hash value: {hash}")  # noqa: G004
     tensor = tensor.cpu()
     buf = tensor.numpy().tobytes()[:tensor_size]
     return _unpickler(io.BytesIO(buf)).load()
